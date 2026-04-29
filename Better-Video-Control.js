ad9166987972c2437a11eb113e544f39a57f526f
Serhii Kravchenko <htmlcoder4u@gmail.com>
2026-04-30 01:40:24 +0300

    Update Better-Video-Control.js

--- Better-Video-Control.js
+++ Better-Video-Control.js
@@ -1,4 +1,4 @@
-/ ==UserScript==
+// ==UserScript==
 // @name Better Video Control
 // @namespace http://tampermonkey.net/
 // @version 2026-04-12
@@ -12,51 +12,52 @@
 // ==/UserScript==
 
 (function() {
-'use strict';
-const getVideoTag = () => document.querySelector('*[data-testid=StreamElement] video');
-document.addEventListener('keyup', function (e) {
-    const videoTag = getVideoTag();
+    'use strict';
+    const getVideoTag = () => document.querySelector('*[data-testid=StreamElement] video');
+    document.addEventListener('keyup', function (e) {
+        const videoTag = getVideoTag();
 
-    if (videoTag) {
-        const currentSpeed = videoTag.playbackRate;
-        const maxPosibleSpeed = 15;
-        const isTargetNotInput = !e.target.matches('input, textarea, select');
-        let newSpeed = null;
+        if (videoTag) {
+            const currentSpeed = videoTag.playbackRate;
+            const maxPosibleSpeed = 15;
+            const isTargetNotInput = !e.target.matches('input, textarea, select');
+            let newSpeed = null;
 
-        if (isTargetNotInput) {
-            if (!isNaN(parseInt(e.key))) {
-                newSpeed = parseInt(e.key);
-            }
+            if (isTargetNotInput) {
+                if (!isNaN(parseInt(e.key))) {
+                    newSpeed = parseInt(e.key);
+                }
 
-            if ((e.key === '=' || e.key === '+') && !e.shiftKey) {
-                newSpeed = parseInt(currentSpeed < maxPosibleSpeed ? currentSpeed + 1 : maxPosibleSpeed);
-            }
+                if ((e.key === '=' || e.key === '+') && !e.shiftKey) {
+                    newSpeed = parseInt(currentSpeed < maxPosibleSpeed ? currentSpeed + 1 : maxPosibleSpeed);
+                }
 
-            if (e.key === '-' && !e.shiftKey) {
-                newSpeed = currentSpeed >= 1 ? currentSpeed - 1 : currentSpeed;
-            }
+                if (e.key === '-' && !e.shiftKey) {
+                    newSpeed = currentSpeed >= 1 ? currentSpeed - 1 : currentSpeed;
+                }
 
-            if (e.key === '*') {
-                newSpeed = maxPosibleSpeed;
-            }
+                if (e.key === '*') {
+                    newSpeed = maxPosibleSpeed;
+                }
 
-            if (newSpeed !== null) {
-                console.log('video playback rate changed to:', newSpeed);
+                if (newSpeed !== null) {
+                    console.log('video playback rate changed to:', newSpeed);
 
-                videoTag.playbackRate = newSpeed === 0 ? 0.5 : newSpeed;
+                    videoTag.playbackRate = newSpeed === 0 ? 0.5 : newSpeed;
+                }
             }
         }
-    }
-})
+    });
 
-document.addEventListener('wheel', function(e) {
-    const videoTag = getVideoTag();
-    const currentSpeed = videoTag.playbackRate;
+    document.addEventListener('wheel', function(e) {
+        const videoTag = getVideoTag();
+        const currentSpeed = videoTag.playbackRate;
 
-    if (e.target === videoTag) {
-        const currentTime = videoTag.currentTime;
-        const deltaNormalized = e.deltaY / 100; // convert to 1 or -1, by default 100 and -100
+        if (e.target === videoTag) {
+            const currentTime = videoTag.currentTime;
+            const deltaNormalized = e.deltaY / 100; // convert to 1 or -1, by default 100 and -100
 
-        videoTag.currentTime = currentTime + deltaNormalized * currentSpeed;
-    }
-})
+            videoTag.currentTime = currentTime + deltaNormalized * currentSpeed;
+        }
+    });
+});
